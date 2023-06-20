7975c34198b2dc73cdcbbf5295a88d373421d5dd
KhaledAbdellaty <kabdellaty@gmail.com>
2023-06-20 07:56:17 -0700

    a function that prints the last digit of a number.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
  * _abs - a function that computes the absolute value of an integer.
- * @int: an argumant
+ * @n: an argumant
  * Return: returns an integer number
  */
 int _abs(int n)
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_last_digit - a function that prints the last digit of a number.
+ * @n: an argumant
+ * Return: returns last digit number
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+		x = -1 * (n % 10)
+	else
+		x = n % 10;
+
+	return (x);
+}
