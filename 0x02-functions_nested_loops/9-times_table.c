5184e373f0a53081f97a758482bccbe3b4a7e96b
KhaledAbdellaty <kabdellaty@gmail.com>
2023-06-20 12:12:01 -0700

    correct answer

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,8 +17,8 @@ void times_table(void)
 			if (prod < 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '10');
-			_putchar((prod % 10) + '10');
+				_putchar((prod / 10) + 10);
+			_putchar((prod % 10) + 10);
 		}
 		_putchar('\n');
 	}
