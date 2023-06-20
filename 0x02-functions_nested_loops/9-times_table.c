002a3e793051674d5950588270a32b051e6d0901
KhaledAbdellaty <kabdellaty@gmail.com>
2023-06-20 12:28:30 -0700

    fix checker

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,21 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar ('0');
-		for (mult = 0; mult <= 9; mult++)
+		_putchar(',');
+		_putchar(' ');
+		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar (',');
-			_putchar (' ');
 			prod = num * mult;
-			if (prod < 9)
-				_putchar(' ');
+			if ((prod / 10) > 0)
+				_putchar((d / 10) + '0');
 			else
-				_putchar((prod / 10) + 10);
-			_putchar((prod % 10) + 10);
+				_putchar(' ');
+			_putchar((prod % 10) + '0');
+			if (mult < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
