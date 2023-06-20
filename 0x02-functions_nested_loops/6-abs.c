01e185f2a89e1b0a59ced2ce278841df4ee3f04a
KhaledAbdellaty <kabdellaty@gmail.com>
2023-06-20 07:39:37 -0700

    a function that prints the last digit of a number.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer.
+ * @int: an argumant
+ * Return: returns an integer number
+ */
+int _abs(int)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
