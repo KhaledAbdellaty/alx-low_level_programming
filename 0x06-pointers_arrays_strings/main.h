189c412df22587695226bae0d7264ec218e234ea
KhaledAbdellaty <kabdellaty@gmail.com>
2023-06-29 12:53:19 -0700

    fix answer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,4 +5,4 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 void print_array(int *a, int n);
-char *string_toupper(char *);
+char *string_toupper(char *n);
