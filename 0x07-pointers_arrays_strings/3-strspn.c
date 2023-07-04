d90e1f07763469b69af8b913ecd5306272202c57
KhaledAbdellaty <kabdellaty@gmail.com>
2023-07-04 09:10:09 -0700

    Returns the number of bytes in the initial segment of s

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,6 +3,7 @@
  * _strspn - a function that gets the length of a prefix substring.
  * @s: the first string
  * @accept: the second string
+ * Return: Returns the number of bytes in the initial segment of s
  */
 unsigned int _strspn(char *s, char *accept)
 {
