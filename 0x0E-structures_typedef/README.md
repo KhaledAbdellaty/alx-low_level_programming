253dae8fe26dfcd8cf644b34e251c83c91ed57ac
KhaledAbdellaty <kabdellaty@gmail.com>
2023-07-17 20:46:06 -0700

    a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this project for strucs and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef CHECK_DOG
+#define CHECK_DOG
+
+/**
+ * struct dog - is a new data type holds
+ * dog information
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ */
+struct dog
+{
+	char *name, owner;
+	int age;
+}
+#endif
