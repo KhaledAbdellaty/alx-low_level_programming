7a189f34d5a5c4dca8e535012adb25ead8d3d63a
KhaledAbdellaty <kabdellaty@gmail.com>
2023-07-21 11:11:45 -0700

    a function that prints numbers, followed by a new line.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef CHECKER
 #define CHECKER
 #include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
