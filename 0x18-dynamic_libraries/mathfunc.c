0035065ed5e6f175f9db7dcb5c3e7af79b93aa49
KhaledAbdellaty <kabdellaty@gmail.com>
2023-09-18 09:12:35 -0700

    a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/mathfunc.c
@@ -0,0 +1,26 @@
+#include "math.h"
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
+int sub(int a, int b)
+{
+        int res = a - b;
+        return (res);
+}
+int mul(int a, int b)
+{
+        int res = a * b;
+        return (res);
+}
+int div(int a, int b)
+{
+        int res = a / b;
+        return (res);
+}
+int mod(int a, int b)
+{
+        int res = a % b;
+        return (res);
+}
